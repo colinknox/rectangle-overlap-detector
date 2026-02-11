b07d0d9df60c2127b50130f5fc91889795c8b2d3
ColinKnox <colinwknox@gmail.com>
2026-02-11 11:28:10 -0500

    worked on Rectangle class

--- main.py
+++ main.py
@@ -1,2 +1,25 @@
 class Rectangle:
-    pass
+    def __init__(self, x1, y1, x2, y2):
+        self.x1 = x1
+        self.y1 = y1
+        self.x2 = x2
+        self.y2 = y2
+
+    def get_left_x(self):
+        return min(self.x1, self.x2)
+    
+    def get_right_x(self):
+        return max(self.x1, self.x2)
+    
+    def get_bottom_y(self):
+        return min(self.y1, self.y2)
+    
+    def get_top_y(self):
+        return max(self.y1, self.y2)
+
+
+
+rectangle = Rectangle(6, 3, 7, 4)
+
+print(f"DEBUG: Get Bottom Y = {rectangle.get_bottom_y()}")
+print(f"DEBUG: Get Right X = {rectangle.get_top_y()}")
